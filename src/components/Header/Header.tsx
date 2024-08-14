42038c8298e3b808b6f086d4a0df45ce2e4a7b2e
Pedro Chagas <pedrochagas.sm@gmail.com>
2024-08-14 22:08:15 +0000

    Added more images to header, click to change too

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,26 @@
 import "./Header.css"
 import mandelbrot from "../../assets/mandelbrot.png"
+import old_man from "../../assets/old_man.png"
+import bear from "../../assets/bear.png"
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
+function changeCurrentPic(index: number, picturesLen: number){
+    if (index != picturesLen-1){
+        return index+1 
+    }
+    return 0
+}
 
 function Header(){
+    const pictures = [mandelbrot, old_man, bear]
+    const [current_pic, setCurrent_pic] = useState(0)
     return (
     <div className="Header_div">
         <Link to={"/"}><h1>Pedro Chagas</h1></Link>
-        <img src={mandelbrot}/>
+        <img onClick={() =>
+                setCurrent_pic(changeCurrentPic(current_pic, pictures.length))}
+                src={pictures[current_pic]}/>
     </div>
     );
 }
