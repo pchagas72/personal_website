88c4e9bb908b026ed9b95653d07936cf29f90169
Pedro Chagas <pedrochagas.sm@gmail.com>
2025-01-14 13:10:16 +0000

    Corrected typos

--- src/routes/Resume.tsx
+++ src/routes/Resume.tsx
@@ -22,7 +22,7 @@ function Resume(){
                         I'm a strong team worker, with a gift for leadership and always working for the best of my team and project.
                         I'm also a natural born innovator! I'll always search for the best new ways to develop, interact and produce.
                     </p>
-                    <p> I want to <a className="prettyLink">make real difference</a> on <a className="prettyLink"> on real people.</a></p>
+                    <p> I want to <a className="prettyLink">make a real difference</a> on <a className="prettyLink"> in real people's lives.</a></p>
                 </section>
             </div>
 
@@ -32,27 +32,28 @@ function Resume(){
                     <h2 className="section_title">Soft skills</h2>
                     <ul>
                         <li> Great team worker </li>
-                        <li> Agile student, scrum master </li>
+                        <li> Agile learner, Scrum Master </li>
                         <li> Leader with passion for diffcult projects </li>
                         <li> Learning on-demand </li>
                         <li> Public speaker with assertive dialogue courses </li>
                         <li> Passionate teacher </li>
+                        <li> Researcher and innovator </li>
                         <li> Delegate on MUN simulations </li>
 
                     </ul>
                     <h2 className="section_title">Programming languages</h2>
                     <ul>
                         <li> Systems Programming with C/C++ and Rust </li>
-                        <li> AI developer, pytorch, TensorFlow and low-level</li>
-                        <li> Scripting with GoLang, Python and Lua </li>
+                        <li> AI developer, PyTorch, TensorFlow and low-level development</li>
+                        <li> Scripting with Go, Python and Lua </li>
                         <li> Web development with JS/TS</li>
                         <li> Linux distro maintainer, and linux server admin </li>
-                        <li> Game and physiscs engines programming </li>
+                        <li> Game and physics engine programming </li>
                     </ul>
                     <h2 className="section_title">Frameworks</h2>
                     <ul>
                         <li>Web development with React and Django </li>
-                        <li>Internal servers with ngrok and linux internal networks</li>
+                        <li>Internal servers with ngrok and Linux internal networks</li>
                     </ul>
                 </section>
             </div>
@@ -66,7 +67,7 @@ function Resume(){
                             I'm a robotics coach and teacher for the Military School of Recife. I thrive for the success of my team.
                         </p>
                         <p>
-                            I'm a robotics coach and teacher for the Military School of Recife. I thrive for the success of my team.
+                            I also give online lessons for free for the new teams under the national system of military schools.
                         </p>
                     </div>
                 </section>
@@ -90,10 +91,10 @@ function Resume(){
                     <div>
                         <h2 className="section_title">Guitar Teacher - Online english lessons / Recife-PE</h2>
                         <p>
-                            I give guitar lessons both in english and portuguese for guitar starters.
+                            I provide guitar lessons in both English and Portuguese for berginners.
                         </p>
                         <p>
-                            And starting in 2024, I gave guitar lessons for free to young people in my neighborhood, aiming to increase the transforming power of music. Now I'm looking for a church or nonprofit organization to start a new project.
+                            Starting in 2024, I gave guitar lessons for free to young people in my neighborhood, aiming to increase the transforming power of music. Now I'm looking for a church or nonprofit organization to start a new project.
                         </p>
                     </div>
                 </section>
@@ -101,7 +102,7 @@ function Resume(){
                     <div>
                         <h2 className="section_title">Young Entrepeneur</h2>
                         <p>
-                            I have a sidekick business in which me and a group of friends develop websites and help college students with programming subjects and projects.
+                            I run a side business in which me and a group of friends develop websites and help college students with programming subjects and projects.
                         </p>
                     </div>
                 </section>
@@ -109,7 +110,7 @@ function Resume(){
                     <div>
                         <h2 className="section_title">Delegate in MUN simulation / PEMUN/CRIJN</h2>
                         <p>
-                            I take part in MUN simulations as speach advisor, and tech lead.
+                            I take part in MUN simulations as speech advisor, and tech lead.
                         </p>
                     </div>
                 </section>
@@ -134,8 +135,8 @@ function Resume(){
                         <li>3rd place - IBM Quantum Challenge (2023)</li>
                         <li>1st place - Globo Hackathon Challenge, Rec'n Play (2024)</li>
                         <li>Total of three gold medals in national robotics competitions as student and as teacher</li>
-                        <li>Best innovator prize at the national robotics olympiad</li>
-                        <li>Best programmer and robotics club president of the miltary school system of Brazil</li>
+                        <li>Best Innovator Prize at the National Robotics Olympiad</li>
+                        <li>Best programmer and robotics club president of the military school system of Brazil</li>
                         <li>First place at FEBRACE 2021</li>
                         <li>Second place at MOSTRATEC 2022</li>
                     </ul>
